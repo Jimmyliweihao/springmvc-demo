f642327cd2950b02db5adb5452bfff073e34ef25
liweihao <liweihao8447@gmail.com>
2025-09-15 17:02:06 +0800

    lzm-1.0

---
+++ src/test/java/com/itheima/springmvcdemo/SpringmvcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.springmvcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
